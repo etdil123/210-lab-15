f5b33fd48b16d0d15649defbffc4d543facbb63c
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-09-29 13:23:55 -0700

    Added all member functions

--- Lab_14_Color_Class.cpp
+++ Lab_14_Color_Class.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+const int WIDTH = 15;
 
 class Color{
+    // private member attributes 
     private:
         int red;
         int green;
         int blue;
 
-    
-};
+    public:
+        // getters
+        int getRed() { return red; }
+        int getGreen() { return green; }
+        int getBlue() {return blue; }
+        
+        // setters
+        void setRed(int r) { red = r; }
+        void setBlue(int b) { blue = b; }   
+        void setGreen(int g) { green = g; }
 
-
+        // Color print method
+        void colorPrint(){
+            cout << setw(WIDTH) << "Red: " << red << endl;
+            cout << setw(WIDTH) << "Green: " << green << endl;
+            cout << setw(WIDTH) << "Blue: " << blue << endl;
+        }
+};  
 
 int main(){
 
