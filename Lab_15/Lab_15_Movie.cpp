dd35551a1cf5809970eb8c0a42c3082db24ba1a8
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-09-29 14:51:54 -0700

    Class Updates

--- Lab_15/Lab_15_Movie.cpp
+++ Lab_15/Lab_15_Movie.cpp
@@ -1,5 +1,35 @@
 // COMSC 210 | Lab 15: Movie Class | Ethan Dilk
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <string>
+
 using namespace std;
 
+const int WIDTH = 10;
+
+class Movie {
+    private:
+        string title;
+        string screenwriter;
+        string releaseYear;
+
+    public:
+        // getters
+        string getTitle() { return title; }
+        string getScreenwriter() {return screenwriter; }
+        string getReleaseYear() {return releaseYear; }
+
+        // setters
+        void setTitle(string title) {this->title = title;}
+        void setScreenwriter(string screenwriter) {this->screenwriter = screenwriter;}
+        void setReleaseYear(string releaseYear) {this->releaseYear = releaseYear; } 
+
+        // print method to display object attributes
+        void print(){
+            cout << "Movie: " << title << endl;
+            cout << setw(WIDTH) << "Year Released: " << releaseYear << endl;
+            cout << setw(WIDTH) << "Screenwriter: " << screenwriter << endl;
+        }
+
+};
